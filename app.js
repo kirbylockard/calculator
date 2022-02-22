cc4217823f75c0f494e82b14e4e101713f2d71b6
Kirby Lockard <kirbylockard@gmail.com>
2022-02-21 17:49:24 -0700

    reached previous functionality

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 -----PHASE 1-----
 -floating point precision issue
 -limit length of string to container 
+-keep solution when -/+ is pressed
 -need to be able to switch operation
     5+-(num to subtract) switch from + operation to -
     add class to button for depressed action
@@ -10,31 +11,36 @@
 -2.2222222e15
 
 -Add data-operation attribute to buttons to bring in possibility to reduce code.
--something like--------
-operations = SELECTELEMENTSBYCLASS?
-for operation in operationsList{
-  operation.addEventListener('click', () => {
-  if (nums.length == 0) {
-    nums.push(parseFloat(display.innerHTML));
-    operation = operation.getAttribute("data-operation");
-    resetDispMsg();
-  } else if (nums.length == 1) {
-    nums.push(parseFloat(display.innerHTML));
-    solve();
-    operation = operation.getAttribute("data-operation");
-    nums.push(parseFloat(display.innerHTML));
-    resetDispMsg();
-  } else if (parseFloat(display.innerHTML == solution)) {
-    nums.push(solution);
-    operation = operation.getAttribute("data-operation");
-    resetDispMsg();
-  }
-})
+--------------- STANDARD MATH(two operands) + - / * ^ ---------------
+on click{
+  1. if operation !="" => reset new operation, remove current opp class, apply new current opp class
+  2. if no numbers=> save num, save opp, add current opp class, reset dispMsg
+  3. if 1 number=> save 2nd num, solve, nums = solution, save new opp, add current opp class, reset dispMsg
+  4. if display == solution=> nums = solution, save new opp, add current opp class, reset dispMSG 
 }
+--------------- SINGLE OPERAND MATH exponent --------------- 
+on click {
+  1. if operation !="" => reset new operation, remove current opp class, apply new current opp class FOR BRIEF TIME, save num, solve 
+  2. if display = solution => nums = solution, class change, solve
+  3. if nums.length == 0 => save num, change class, solve
+
+}
+--------------- DISPLAY EFFECT +/- . CLR --------------- 
+three separate code blocks due to different functionality differences
+
+--------------- SOLVE = --------------- 
+Individual due to functionality
+
+--------NEEDED FUNCTIONS--------
+checkOpp?
+assignOpp?
+saveSolution?
+addClass?
+removeClass?
+
+
 
 -----PHASE 2-----
--OOP for buttons?
--Refactor code where possible
 -Media Queries
 
 -----PHASE 3-----
@@ -57,24 +63,45 @@ let clrNum = 0;
 
 //CAN THESE BE TURNED INTO OBJECTS LATER??
 const numBtns = document.getElementsByClassName('numBtn');
+const doubleOperands = document.getElementsByClassName('doubleOperand');
 const display = document.getElementById('display');
 const equals = document.getElementById('equals');
 const clr = document.getElementById('clr');
-const multiply = document.getElementById('mult');
-const divide = document.getElementById('divide');
-const subtract = document.getElementById('subtract');
-const add = document.getElementById('add');
-const exponent = document.getElementById('exp');
+//const multiply = document.getElementById('mult');
+//const divide = document.getElementById('divide');
+//const subtract = document.getElementById('subtract');
+//const add = document.getElementById('add');
+//const exponent = document.getElementById('exp');
 const squareRoot = document.getElementById('sqr-root');
 const plusMinus = document.getElementById('plus-minus');
 
 /*-----FUNCTIONS-----*/
 
-const saveNum = () => nums.push(parseFloat(dispMsg));
+const saveNum = () => nums.push(parseFloat(display.innerHTML));
 
 const err = function() {
   resetVars();
   display.innerHTML = 'ERR';
+}
+
+const resetVars = function() {
+  dispMsg = '';
+  nums = [];
+  operation = '';
+  clrNum = 0;
+}
+
+const resetDisplay = function() {
+  display.innerHTML = '0';
+}
+
+const resetDispMsg = function () {
+  dispMsg = '';
+}
+
+const resetAll = function () {
+  resetVars();
+  resetDisplay();
 }
 
 const solve = function() {
@@ -107,36 +134,19 @@ const solve = function() {
   }
 }
 
-const resetVars = function() {
-  dispMsg = '';
-  nums = [];
-  operation = '';
-  clrNum = 0;
-}
-
-const resetDisplay = function() {
-  display.innerHTML = '0';
-}
-
-const resetDispMsg = function () {
-  dispMsg = '';
-}
-
-const resetAll = function () {
-  resetVars();
-  resetDisplay();
-}
-
 
 /*-----POGRAM-----*/ 
 
 //ASSIGN NUMBER BUTTON EVENTS
 for (let i = 0; i < numBtns.length; i++) {
   let num = numBtns[i];
-  // if (display.innerHTML == solution) {
-  //   resetDisplay();
-  // }
+  if (display.innerHTML == solution) {
+    resetDisplay();
+    
+  }
   num.addEventListener('click', () => {
+    solution = undefined;
+
     if (num.innerHTML == "." && dispMsg.includes(".")) {
       return;
     } else if (num.innerHTML =="." && dispMsg == '') {
@@ -148,6 +158,47 @@ for (let i = 0; i < numBtns.length; i++) {
     }
   })
 }
+
+//ASSIGN DOUBLE OPERAND EVENTS
+
+for (let i = 0; i < doubleOperands.length; i++) {
+  let currentOpp = doubleOperands[i];
+  let currentOppAssigned = currentOpp.dataset.operation; 
+  currentOpp.addEventListener('click', () => {
+    console.log(`clicked ${currentOppAssigned}`);
+    //how to change operation without saving num and solving first
+    // if (operation) {
+    //   operation = currentOppAssigned;
+    //   console.log(`change operation to ${currentOppAssigned}`);
+    // } else 
+    if (nums.length == 0) {
+      saveNum()
+      operation = currentOppAssigned;
+      resetDispMsg();
+    } else if (nums.length == 1) {
+      saveNum()
+      solve();
+      operation = currentOppAssigned;
+      saveNum()
+      resetDispMsg();
+    } else if (parseFloat(display.innerHTML == solution)) {
+      nums.push(solution);
+      operation = currentOppAssigned;
+      resetDispMsg();
+    }
+    console.log(`nums are ${nums}`);
+    console.log(`operation is ${operation}`);
+    console.log(`solution is ${solution}`);
+  })
+}
+
+
+/*on click{
+  1. if operation !="" => reset new operation, remove current opp class, apply new current opp class
+  2. if no numbers=> save num, save opp, add current opp class, reset dispMsg
+  3. if 1 number=> save 2nd num, solve, nums = solution, save new opp, add current opp class, reset dispMsg
+  4. if display == solution=> nums = solution, save new opp, add current opp class, reset dispMSG 
+}*/
 
 //ASSIGN ACTION BUTTON EVENTS
 clr.addEventListener('click', () => {
@@ -167,95 +218,107 @@ equals.addEventListener('click', () => {
   } 
 })
 
-add.addEventListener('click', () => {
-  if (nums.length == 0) {
-    nums.push(parseFloat(display.innerHTML));
-    operation = "add";
-    resetDispMsg();
-  } else if (nums.length == 1) {
-    nums.push(parseFloat(display.innerHTML));
-    solve();
-    operation = "add";
-    nums.push(parseFloat(display.innerHTML));
-    resetDispMsg();
-  } else if (parseFloat(display.innerHTML == solution)) {
-    nums.push(solution);
-    operation = "add";
-    resetDispMsg();
+plusMinus.addEventListener('click', () => {
+  if (dispMsg.charAt(0) != "-") {
+    dispMsg = "-" + dispMsg;
+    display.innerHTML = dispMsg;
+  } else if (dispMsg.charAt(0) == "-") {
+    dispMsg = dispMsg.substring(1);
+    display.innerHTML = dispMsg;
   }
 })
 
-subtract.addEventListener('click', () => {
-  if (nums.length == 0) {
-    nums.push(parseFloat(display.innerHTML));
-    operation = "subtract";
-    resetDispMsg();
-  } else if (nums.length == 1) {
-    nums.push(parseFloat(display.innerHTML));
-    solve();
-    operation = "subtract";
-    nums.push(parseFloat(display.innerHTML));
-    resetDispMsg();
-  } else if (parseFloat(display.innerHTML == solution)) {
-    nums.push(solution);
-    operation = "subtract";
-    resetDispMsg();
-  }
-})
 
-multiply.addEventListener('click', () => {
-  if (nums.length == 0) {
-    nums.push(parseFloat(display.innerHTML));
-    operation = "multiply";
-    resetDispMsg();
-  } else if (nums.length == 1) {
-    nums.push(parseFloat(display.innerHTML));
-    solve();
-    operation = "multiply";
-    nums.push(parseFloat(display.innerHTML));
-    resetDispMsg();
-  } else if (parseFloat(display.innerHTML == solution)) {
-    nums.push(solution);
-    operation = "multiply";
-    resetDispMsg();
-  }
-})
 
-divide.addEventListener('click', () => {
-  if (nums.length == 0) {
-    nums.push(parseFloat(display.innerHTML));
-    operation = "divide";
-    resetDispMsg();
-  } else if (nums.length == 1) {
-    nums.push(parseFloat(display.innerHTML));
-    solve();
-    operation = "divide";
-    nums.push(parseFloat(display.innerHTML));
-    resetDispMsg();
-  } else if (parseFloat(display.innerHTML == solution)) {
-    nums.push(solution);
-    operation = "divide";
-    resetDispMsg();
-  }
-})
+// add.addEventListener('click', () => {
+//   if (nums.length == 0) {
+//     nums.push(parseFloat(display.innerHTML));
+//     operation = "add";
+//     resetDispMsg();
+//   } else if (nums.length == 1) {
+//     nums.push(parseFloat(display.innerHTML));
+//     solve();
+//     operation = "add";
+//     nums.push(parseFloat(display.innerHTML));
+//     resetDispMsg();
+//   } else if (parseFloat(display.innerHTML == solution)) {
+//     nums.push(solution);
+//     operation = "add";
+//     resetDispMsg();
+//   }
+// })
 
-exponent.addEventListener('click', () => {
-  if (nums.length == 0) {
-    nums.push(parseFloat(display.innerHTML));
-    operation = "exponent";
-    resetDispMsg();
-  } else if (nums.length == 1) {
-    nums.push(parseFloat(display.innerHTML));
-    solve();
-    operation = "exponent";
-    nums.push(parseFloat(display.innerHTML));
-    resetDispMsg();
-  } else if (parseFloat(display.innerHTML == solution)) {
-    nums.push(solution);
-    operation = "exponent";
-    resetDispMsg();
-  }
-})
+// subtract.addEventListener('click', () => {
+//   if (nums.length == 0) {
+//     nums.push(parseFloat(display.innerHTML));
+//     operation = "subtract";
+//     resetDispMsg();
+//   } else if (nums.length == 1) {
+//     nums.push(parseFloat(display.innerHTML));
+//     solve();
+//     operation = "subtract";
+//     nums.push(parseFloat(display.innerHTML));
+//     resetDispMsg();
+//   } else if (parseFloat(display.innerHTML == solution)) {
+//     nums.push(solution);
+//     operation = "subtract";
+//     resetDispMsg();
+//   }
+// })
+
+// multiply.addEventListener('click', () => {
+//   if (nums.length == 0) {
+//     nums.push(parseFloat(display.innerHTML));
+//     operation = "multiply";
+//     resetDispMsg();
+//   } else if (nums.length == 1) {
+//     nums.push(parseFloat(display.innerHTML));
+//     solve();
+//     operation = "multiply";
+//     nums.push(parseFloat(display.innerHTML));
+//     resetDispMsg();
+//   } else if (parseFloat(display.innerHTML == solution)) {
+//     nums.push(solution);
+//     operation = "multiply";
+//     resetDispMsg();
+//   }
+// })
+
+// divide.addEventListener('click', () => {
+//   if (nums.length == 0) {
+//     nums.push(parseFloat(display.innerHTML));
+//     operation = "divide";
+//     resetDispMsg();
+//   } else if (nums.length == 1) {
+//     nums.push(parseFloat(display.innerHTML));
+//     solve();
+//     operation = "divide";
+//     nums.push(parseFloat(display.innerHTML));
+//     resetDispMsg();
+//   } else if (parseFloat(display.innerHTML == solution)) {
+//     nums.push(solution);
+//     operation = "divide";
+//     resetDispMsg();
+//   }
+// })
+
+// exponent.addEventListener('click', () => {
+//   if (nums.length == 0) {
+//     nums.push(parseFloat(display.innerHTML));
+//     operation = "exponent";
+//     resetDispMsg();
+//   } else if (nums.length == 1) {
+//     nums.push(parseFloat(display.innerHTML));
+//     solve();
+//     operation = "exponent";
+//     nums.push(parseFloat(display.innerHTML));
+//     resetDispMsg();
+//   } else if (parseFloat(display.innerHTML == solution)) {
+//     nums.push(solution);
+//     operation = "exponent";
+//     resetDispMsg();
+//   }
+// })
 
 squareRoot.addEventListener('click', () => {
   if (parseFloat(display.innerHTML == solution)) {
@@ -277,12 +340,3 @@ squareRoot.addEventListener('click', () => {
   }
 })
 
-plusMinus.addEventListener('click', () => {
-  if (dispMsg.charAt(0) != "-") {
-    dispMsg = "-" + dispMsg;
-    display.innerHTML = dispMsg;
-  } else if (dispMsg.charAt(0) == "-") {
-    dispMsg = dispMsg.substring(1);
-    display.innerHTML = dispMsg;
-  }
-})
